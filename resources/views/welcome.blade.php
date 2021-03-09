0855f48f1d127acc92a5fb87d6c1e84e3a72f2a2
Darylabrador <abrador.daryl@gmail.com>
2021-03-09 13:52:05 +0400

    Fix front

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
 @section('content')
     <div class="mt-5 mx-auto container shadow p-3">
         <h6> <span id="userConnected"> </span> connecté(s)</h6>
-        <div id="messagerie" class="p-2 border rounded border-dark" style="max-height: 300px !important; overflow-y: scroll;"></div>
+        <div id="messagerie" class="p-2 border rounded border-dark" style="max-height: 300px !important; min-height: 300px !important;overflow-y: scroll;"></div>
         <form class="my-3" id="messageForm">  
             <textarea id="contentMessage" cols="30" rows="5" class="form-control" required></textarea>
             <div class="d-flex justify-content-end mt-3">
